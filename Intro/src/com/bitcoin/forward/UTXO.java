0a5096271eccf5d5fdd57e8318fd168549017db1
winnieryl <winnieryl@gmail.com>
2018-01-08 22:12:16 +0800

    test

--- Intro/src/com/bitcoin/forward/UTXO.java
+++ Intro/src/com/bitcoin/forward/UTXO.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class UTXO implements Comparable<UTXO> {
 
     /** Hash of the transaction from which this UTXO originates */
+	/**test**/
     private byte[] txHash;
 
     /** Index of the corresponding output in said transaction */
